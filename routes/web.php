de5ff117e236eefb232f254b6dd11377a9c485e0
Paul Oehler <poehler@interworx.com>
2021-12-06 20:10:14 -0500

    Implement delete functino for the question and show

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('questions', QuestionController::class);
+Route::resource('questions', QuestionController::class)->except('show');
+Route::get('/questions/{slug}', [QuestionController::class, 'show'])->name('questions.show');
